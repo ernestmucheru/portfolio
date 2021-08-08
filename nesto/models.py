69a3a4778febeb4c37c7f3f41346b8f24bbd654b
Ernest Mucheru <ernestmucheru254@gmail.com>
2021-08-08 23:21:42 +0300

    created portfolio models

--- nesto/models.py
+++ nesto/models.py
@@ -1,17 +1,38 @@
 from django.db import models
+from django.db.models.fields.files import ImageField
 
 # Create your models here.
-class About(models.model):
+class About(models.Model):
     description = models.TextField()
-    top_skills = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = "About Me"
+        verbose_name_plural = "About Me"
+
+    def __str__(self):
+        return "About Me"
 
 class Achievement(models.Model):
     project_count = models.CharField(max_length=15)
+
+    class Meta:
+        verbose_name = "Achievement"
+        verbose_name_plural = "Achievements"
+
+    def __str__(self):
+        return "Achievements"
 
 class Expertise(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField()
     icon = models.ImageField()
+
+    class Meta:
+        verbose_name = "Expertise"
+        verbose_name_plural = "Expertise"
+
+    def __str__(self):
+        return self.title
 
 class Banner(models.Model):
     coffee=models.IntegerField()
@@ -19,10 +40,37 @@ class Banner(models.Model):
     clients = models.IntegerField()
     patners = models.IntegerField()
 
+    class Meta:
+        verbose_name = "Banner"
+        verbose_name_plural = "Banner"
+
+    def __str__(self):
+        return "Banner"
+
 class Education(models.Model):
     course = models.CharField(max_length=50)
     description = models.TextField()
 
+    def __str__(self):
+        return Education
+
 class WorkExperience(models.Model):
     role = models.CharField(max_length=50)
     description = models.TextField()
+
+    def __str__(self):
+        return "role"
+
+class Projects(models.Model): 
+    image = models.ImageField()
+    title = models.CharField(max_length=30)
+    livelink = models.URLField(max_length=120)
+    alt = models.CharField(max_length=30)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+
+    def __str__(self):
+        return "title"
