117ed4b4a0d33dce65101e41f225e6727bc414cf
Ernest Mucheru <ernestmucheru254@gmail.com>
2021-08-08 08:34:31 +0300

    created my basic models structure

--- nesto/models.py
+++ nesto/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class About(models.model):
+    description = models.TextField()
+    top_skills = models.CharField(max_length=30)
+
+class Achievement(models.Model):
+    project_count = models.CharField(max_length=15)
+
+class Expertise(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    icon = models.ImageField()
+
+class Banner(models.Model):
+    coffee=models.IntegerField()
+    projects_done = models.IntegerField()
+    clients = models.IntegerField()
+    patners = models.IntegerField()
+
+class Education(models.Model):
+    course = models.CharField(max_length=50)
+    description = models.TextField()
+
+class WorkExperience(models.Model):
+    role = models.CharField(max_length=50)
+    description = models.TextField()
