17778d0ec2eec91b97bbebeb81902cdb83f9453b
Ernest Mucheru <ernestmucheru254@gmail.com>
2021-08-09 06:12:46 +0300

    created the project view

--- nesto/views.py
+++ nesto/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def home(request):
+    about = About.objects.all()
+    achievements = Achievement.objects.all()
+    skills = Expertise.objects.all()
+    banner = Banner.objects.all()
+    courses = Education.objects.all()
+    work = WorkExperience.objects.all()
+    projects = Projects.objects.all()
+
+    context = {'about': about, 'achievements':achievements,'skills':skills, 'banner':banner,'courses':courses, 'work':work,'projects':projects}
+    return render(request, 'portfolio.html', context)
